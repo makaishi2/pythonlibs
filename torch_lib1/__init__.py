525c4cb88c20d574451e2f0af2dc17188cd5cfe8
Masanori Akaishi <masanori.akaishi@gmail.com>
2021-07-04 17:12:20 +0900

    show_predict_resultを落とす

--- torch_lib1/__init__.py
+++ torch_lib1/__init__.py
@@ -148,45 +148,13 @@ def evaluate_history(history):
   plt.show()
 
 
-# 予測結果表示
-def show_predict_result(net, loader, classes, device):
-
-    # DataLoaderから最初の1セットを取得する
-    for images, labels in loader:
-        break
-    n_size = min(len(images), 50)
-
-    # デバイスの割り当て
-    inputs = images.to(device)
-    labels = labels.to(device)
-
-    # 予測計算
-    outputs = net(inputs)
-    predicted = torch.max(outputs,1)[1]
-    images = images.to('cpu')
-
-    # 最初の50個の表示
-    plt.figure(figsize=(20, 15))
-    for i in range(n_size):
-        ax = plt.subplot(5, 10, i + 1)
-        label_name = classes[labels[i]]
-        predicted_name = classes[predicted[i]]
-        img = np.transpose(images[i].numpy(), (1, 2, 0))
-        plt.imshow((img + 1)/2)
-        if label_name == predicted_name:
-            c = 'k'
-        else:
-            c = 'b'
-        ax.set_title(label_name + ':' + predicted_name, c=c)
-        ax.set_axis_off()
-    plt.show()
-
 # イメージとラベル表示
 def show_images_labels(loader, classes, net, device):
 
     # DataLoaderから最初の1セットを取得する
     for images, labels in loader:
         break
+    # 表示数は50個とバッチサイズのうち小さい方
     n_size = min(len(images), 50)
 
     if net is not None:
@@ -199,23 +167,33 @@ def show_images_labels(loader, classes, net, device):
       predicted = torch.max(outputs,1)[1]
       #images = images.to('cpu')
 
-    # 最初の50個の表示
+    # 最初のn_size個の表示
     plt.figure(figsize=(20, 15))
     for i in range(n_size):
         ax = plt.subplot(5, 10, i + 1)
         label_name = classes[labels[i]]
+        # netがNoneでない場合は、予測結果もタイトルに表示する
         if net is not None:
           predicted_name = classes[predicted[i]]
+          # 正解かどうかで色分けをする
           if label_name == predicted_name:
             c = 'k'
           else:
             c = 'b'
           ax.set_title(label_name + ':' + predicted_name, c=c, fontsize=20)
+        # netがNoneの場合は、正解ラベルのみ表示
         else:
           ax.set_title(label_name, fontsize=20)
-        img = np.transpose(images[i].numpy(), (1, 2, 0))
-        plt.imshow((img + 1)/2)
+        # TensorをNumPyに変換
+        image_np = images[i].numpy().copy()
+        # 軸の順番変更 (channel, row, column) -> (row, column, channel)
+        img = np.transpose(image_np, (1, 2, 0))
+        # 値の範囲を[-1, 1] -> [0, 1]に戻す
+        img = (img + 1)/2
+        # 結果表示
+        plt.imshow(img)
         ax.set_axis_off()
+    create_pdf(plt)
     plt.show()
 
 
